95a865dd6c4fb2e1db7253ec87dc08035d79a6ae
Kevin <47866787+Kevinrp22@users.noreply.github.com>
2020-07-17 16:09:30 +0200

    Avanzado hasta clase 75

---
+++ src/components/Products/Product.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { link } from "svelte-routing";
+
+  export let product;
+  const { title, image, price, id } = product;
+</script>
+
+<article class="product">
+  <div class="img-container">
+    <img src={image} alt={title} />
+    <a use:link href={`/product/${id}`} class="btn btn-primary product-link">Detalles</a>
+  </div>
+  <div class="product-footer">
+    <p class="product-title">{title}</p>
+    <p class="product-price">${price}</p>
+  </div>
+</article>
--- src/pages/Products.svelte
+++ src/pages/Products.svelte
@@ -1,1 +1,6 @@
-<h1>Welcome products</h1>
+<script>
+  import Products from "../components/Products/Products.svelte";
+
+</script>
+
+<Products title="Productos"/>
