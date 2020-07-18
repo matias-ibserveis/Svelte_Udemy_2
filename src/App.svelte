f197506de6f375c7fe55c0ad2e3a0d5ae7d22757
matias-ibserveis <matias@ibserveis.com>
2020-07-18 23:04:49 +0200

    hasta clase 65

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,33 @@
 <script>
-	import Login from '.pages/Login.svelte' 
-	import About from '.pages/About.svelte' 
-	import Checkout from '.pages/Checkout.svelte' 
-	import Home from '.pages/Home.svelte' 
-	import Products from '.pages/Products.svelte' 
-	import ProductsTemplate from '.pages/ProductsTemplate.svelte' 
+	import Login from "./pages/Login.svelte";
+	import About from "./pages/About.svelte";
+	import Checkout from "./pages/Checkout.svelte"; 
+	import Home from "./pages/Home.svelte"; 
+	import Products from "./pages/Products.svelte"; 
+	import ProductTemplate from "./pages/ProductTemplate.svelte";
+
+	//router
+	import {Router,Route, Link} from "svelte-routing"
 </script>
 
-	<h1>Hola!</h1>
+<Router>
+
+  <nav class="navbar">
+    <Link to="/">Home</Link>
+    <Link to="/about">About</Link>
+    <Link to="/products">Products</Link>
+    <Link to="/products/5">Prod 5</Link>
+    <Link to="/products/7">Pro 7</Link>
+  </nav>
+
+
+	<Route path="/" component={Home} />
+	<Route path="/about" component={About} />
+	<Route path="/login" component={Login} />
+	<Route path="/products" component={Products} />
+	<Route path="/products/:id" component={ProductTemplate} />
+
+
+
+</Router>
+
--- src/pages/ProductTemplate.svelte
+++ src/pages/ProductTemplate.svelte
@@ -1,1 +1,7 @@
-<h1>Welcome to ProductTemplate page</h1>
+<script>
+export let id
+export let location
+</script>
+
+
+<h1>Welcome to ProductTemplate page with id:{id} </h1>
